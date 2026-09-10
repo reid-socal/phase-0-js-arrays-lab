f456d632f49260537a366c7c4a48800a5fbb29cb
Reid Olson <reid.olson@gmail.com>
2026-09-10 15:13:51 -0700

    did the assignment

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,12 +1,25 @@
 // Write your code here
+let products = ["laptop", "phone", "headphones", "monitor"];
+
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+function addProductName(name) {
+    let newProduct = {
+        name: name,
+}
+
+products.push(newProduct);
+}
+
+function updateProductName(position, newName) {
+  products[position].name = newName;
+}
 
 
+function removeLastProduct() {
+  products.pop(-1);
+}
 
-// Export the necessary parts for testing
-module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-  products
-};
+
